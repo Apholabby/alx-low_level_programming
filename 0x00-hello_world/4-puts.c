d02aa35d441bea142e6b3425c582fee49db89535
apholabby <afolabiadebola236@gmail.com>
2022-09-08 20:09:11 -0700

    add

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints out to the stdout
+ * Description: this program prints a string
+ * Return: ends program
+ */
+int main(void)
+{	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
