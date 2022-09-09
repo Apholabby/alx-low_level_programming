acfdd949b360405c6100a2438e0b98808a5fd576
apholabby <afolabiadebola236@gmail.com>
2022-09-08 20:50:39 -0700

    add

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints out to stdout
+ * Description: this programm prints a string
+ * Return: ends program
+ */
+int main(void)
+{	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}
