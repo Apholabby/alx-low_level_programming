c182ed503ed31d4874508183593b254b27016d6e
apholabby <afolabiadebola236@gmail.com>
2022-09-14 04:25:26 -0700

    add

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,13 @@
 
 /**
  * main - prints alphabets in lower case
- * Description: main prints alphabets
+ * Description: prints alphabets
  * Return: ends program
  */
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a' ; c <= 'z' ; c++)
 		_putchar(c);
 	_putchar('\n');
