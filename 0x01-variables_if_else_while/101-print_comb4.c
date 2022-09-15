e0fe1c197bed1bd59d36934c199ea35d83174b42
apholabby <afolabiadebola236@gmail.com>
2022-09-14 19:29:23 -0700

    add

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints num
+ * Return: ends program
+ */
+int main(void)
+{
+	int n;
+	int i;
+	int h;
+
+	for (h = 0 ; h != 8 ; h++)
+	{
+		i = n + 1;
+		do {
+			putchar('0' + h);
+			putchar('0' + n);
+			putchar('0' + i);
+			if (h != 7)
+			{
+				putchar(',');
+				putchar(32);
+			}
+			i++;
+		} while (i != 10);
+	}
+	putchar('\n');
+	return (0);
+}
