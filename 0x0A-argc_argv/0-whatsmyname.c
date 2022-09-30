a063b4b28de12b9490b6111fa14ef146a9a95289
apholabby <afolabiadebola236@gmail.com>
2022-09-30 12:57:18 -0700

    new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: size of argv
+ * @argv: name of program and other arguments
+ * Return:always o (succsess)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
