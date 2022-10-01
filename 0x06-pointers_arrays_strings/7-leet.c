8895fc7d69ea318cee5d3f7374178d6333145bb5
apholabby <afolabiadebola236@gmail.com>
2022-10-01 06:26:15 -0700

    new commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,10 +14,10 @@ char *leet(char *str)
 		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
 			if (str[indx1] == leet[indx2] ||
-					str[indx1] - 32 == leet[indx2]
+					str[indx1] - 32 == leet[indx2])
 					str[indx1] = indx2 + '0';
 		}
-		indx1++
+		indx1++;
 	}
 	return (str);
 }
