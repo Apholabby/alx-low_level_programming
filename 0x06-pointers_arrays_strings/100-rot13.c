11d8a3ed2a5dd849436c537112a3e669480ce3b2
apholabby <afolabiadebola236@gmail.com>
2022-10-01 07:08:29 -0700

    new commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rot13 - a finction which encodes string with rot13
+ * @s: string to encode
+ * Return: a pointer to serc
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while (((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M')))
+		{
+			s[i] += 13;
+			i++;
+		}
+		if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			s[i] -= 13;
+		i++;
+	}
+	return (s);
+}
