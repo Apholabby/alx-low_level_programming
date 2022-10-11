6f24632f0b6502fb4c678d73954368d01a221c56
apholabby <afolabiadebola236@gmail.com>
2022-10-10 20:30:51 -0700

    new commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,6 +16,6 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %6f\n", d->age);
-		printf("Owner: %\n", d->owner ? d->owner : "(nil)");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
