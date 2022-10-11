54bb4b3e6fd49bedb0f84ec0e380a84c08014b67
apholabby <afolabiadebola236@gmail.com>
2022-10-10 20:20:20 -0700

    new commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * print_dog - prints contents of dog struct
  * @d: some dog struct
@@ -8,9 +9,13 @@
 void print_dog(struct dog *d)
 {
 	if (!d)
+	{
 		return;
-
-	d->name ? printf("Name: %s\n", d->name) : printf("Name: (nil)\n");
-	d->age >= 0 ? printf("Age: %6f\n", d->age) : printf("Age: (nil)\n");
-	d->owner ? printf("Owner: %\n", d->owner) : printf("Owner: (nil)\n");
+	}
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %6f\n", d->age);
+		printf("Owner: %\n", d->owner ? d->owner : "(nil)");
+	}
 }
